e0b47bdc468da4f1f87e41ba7be30e98a526f37d
b729 <mowrey729@gmail.com>
2021-04-25 17:34:23 -0500

    Initial commit basic files deployed

---
+++ index.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    // Your port; if not 3306
+    port: 3306,
+    // Your username
+    user: 'root',
+    // Be sure to update with your own MySQL password!
+    password: 'Lu88ockEm$',
+    database: 'personneldb',
+});
